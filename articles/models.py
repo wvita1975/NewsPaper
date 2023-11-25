596af200c5b1cd57dd3cb2fc4f1e9bd6b996df74
William Vita <williamvita@hotmail.com>
2023-11-25 11:57:35 -0400

    CRUD-create

---
+++ articles/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('article-detail', kwargs={'pk': self.pk})
--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls ), 
     path('accounts/', include("accounts.urls")),
     path('accounts/', include("django.contrib.auth.urls")),
     path('', include("pages.urls")),
+    path('articles/', include("articles.urls")),
 ]
