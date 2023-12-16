3b585bf2da6674adcd0042d85ae844d061c6de28
William Vita <williamvita@hotmail.com>
2023-12-16 11:33:58 -0400

    comments_v01

--- articles/admin.py
+++ articles/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Article
+from .models import Article, Comment
 
 admin.site.register(Article)
+admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -19,3 +19,14 @@ class Article(models.Model):
     
     def get_absolute_url(self):
         return reverse('article_detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_list')
